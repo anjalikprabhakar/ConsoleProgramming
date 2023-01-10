95025df9021e3a8fdc0ec94fa45736d697279ea8
Anjalikprabhakar <anjali.k@edstem.com>
2023-01-10 18:35:46 +0530

    Assignment day2-Atm

---
+++ AdvancedConcepts/Program.cs
@@ -0,0 +1,128 @@
+﻿
+//methods
+
+
+Console.WriteLine("Enter the first Number");
+ int num1 = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Enter the Second Number");
+int num2 = Convert.ToInt32(Console.ReadLine());
+
+ void Add(int a, int b){
+    Console.WriteLine($"Addition : {a + b}");
+}
+
+Add(num1, num2);
+Add(4, 5);
+
+
+int GreaterThan(int a,int b,int c)
+{
+    int largest = a;
+    if (b >largest) {
+    largest= b;
+    }
+    if (c > largest) {
+        largest = c;
+    }
+    return largest;
+}
+
+
+int largest=GreaterThan(4, 5, 6);
+Console.WriteLine( largest);
+
+
+//string
+
+
+string fname = "fstname";
+string lname = "lname";
+
+if (fname.CompareTo(lname) == 0)
+{
+    Console.WriteLine("fstname and  last name areequl");
+}
+
+//arrays
+
+string Tosplit = "a,b,c,d";
+string[] names=Tosplit.Split(',');
+
+int[] marks= new int[5];
+
+for (int i = 0;i <= marks.Length; i++)
+{
+   Console.WriteLine("Enter Marks : ");
+    marks[i]=Convert.ToInt32(Console.ReadLine());
+
+}
+
+for ( int i = 0; i <= marks.Length; i++)
+{
+    Console.WriteLine(marks[i]);
+
+}
+
+
+List <string> devs= new List<string>();
+string dev = string.Empty;
+
+
+while (!dev.Equals("-1"))
+{
+    Console.WriteLine("Enter dev name");
+    dev = Console.ReadLine();
+    if  (dev !=string.Empty && dev.Equals("-1"))
+    devs.Add(dev);
+}
+foreach (string name in devs)
+{
+    Console.WriteLine(name); 
+}
+
+
+
+//datetime
+
+DateTime date = new DateTime();
+Console.WriteLine(date);
+
+DateTime dob= new DateTime(1995,12,13);
+Console.WriteLine(dob);
+
+
+DateTime now = DateTime.Now;
+Console.WriteLine(now.AddHours(1));
+
+
+
+
+//exception handling
+
+Console.WriteLine("Enter the first Number");
+int n1 = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Enter the Second Number");
+int n2 = Convert.ToInt32(Console.ReadLine());
+
+try
+{
+    int c = n1 / n2;
+    Console.WriteLine($"Result is {c}");
+}
+catch (DivideByZeroException e)
+{
+
+    Console.WriteLine($"Illegal operation {e.Message}");
+}
+catch (Exception ex)
+{
+
+    throw ex;
+}
+finally
+{
+    Console.WriteLine("Exit");
+}
+
---
+++ Assignment2Day2/Program.cs
@@ -0,0 +1,104 @@
+﻿Console.WriteLine("Welcome");
+
+int cardNumber = 12345;
+int pinNumber = 1234;
+string name = "name";
+int balance = 10000;
+
+Console.WriteLine("Enter Your 5 Digit ATM CARD Number");
+int inputCardNumber = Convert.ToInt32(Console.ReadLine());
+
+
+Console.WriteLine("Enter Your 4 Digit PIN Number");
+int inputPinNumber = Convert.ToInt32(Console.ReadLine());
+
+bool InputValidation(int inputCardNumber, int inputPinNumber)
+{
+    if (inputPinNumber.ToString().Length != 4)
+    {
+        Console.WriteLine("User pin must be a 4 digit value");
+        return false;
+    }
+    else if (inputCardNumber.ToString().Length != 5)
+    {
+        Console.WriteLine("ATM pin must be a 5 digit value");
+        return false;
+    }
+    else if (inputCardNumber != cardNumber && pinNumber != inputPinNumber)
+    {
+        Console.WriteLine("Invalid credentials");
+        return false;
+    }
+    else
+    {
+        return true;
+    }
+}
+
+bool validation = InputValidation(inputCardNumber, inputPinNumber);
+Console.WriteLine(validation);
+
+int AtmFunction(bool valid)
+{
+    int choice = 0;
+    if (valid is true)
+    {
+        Console.WriteLine($"Helllo {name} !");
+        Console.WriteLine("To check balance Press 1");
+        Console.WriteLine("To withdraw money Press 2");
+        Console.WriteLine("To deposite Money  Press 3");
+        Console.WriteLine("To Cancel Press 4");
+
+        choice = Convert.ToInt32(Console.ReadLine());
+    }
+    return choice;
+}
+int choice = AtmFunction(validation);
+void CheckBalaance(int choice)
+{
+    if (choice == 1)
+    {
+
+        Console.WriteLine($"Your Balance is{balance}");
+    }
+}
+CheckBalaance(choice);
+void Withdraw(int choice)
+{ if (choice == 2)
+    {
+        Console.WriteLine("Enter the Amount To withdraw");
+        int amount = Convert.ToInt32(Console.ReadLine());
+        if (amount > balance)
+        {
+            Console.WriteLine("Insufficient Balance ");
+        }
+        else
+        {
+            Console.WriteLine("Please Collect the cash");
+            Console.WriteLine($"Your Available Balance is {balance - amount}");
+        }
+    }
+}
+Withdraw(choice);
+
+void Deposit(int choice){
+    if (choice == 3)
+    {
+        Console.WriteLine("Enter the Amount To deposit");
+        int amount = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine($"Your Available Balance is {balance + amount}");
+
+    }
+}
+
+Deposit(choice);    
+
+void CancelTransaction(int choice, bool validation)
+{
+    if ((choice==4) ||(validation==false))
+    {
+        Console.WriteLine("Thank you! your Transaction is Cancelled");
+    }
+}
+
+CancelTransaction(choice,validation);  
